e0604423c1a796a8c171834b87939e4a519f98d5
dangerousanimal <hideaki_ishii1204@yahoo.co.jp>
2013-09-12 00:03:40 +0900

    create fileupload application

---
+++ requestHandlers.js
@@ -0,0 +1,64 @@
+var fs = require("fs"),
+    formidable = require("formidable");
+
+ function start(res){
+ 	console.log("Request handler 'start' was called.");
+
+ 	var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" '+
+    'content="text/html; charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<h1>ファイルをアップロードしてください</h1>'+
+    '<form action="/upload" enctype="multipart/form-data" '+
+    'method="post">'+
+    '<input type="file" name="upload">'+
+    '<input type="submit" value="Upload" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+    res.writeHead(200, {"Content-Type":"text/html"});
+    res.write(body);
+    res.end();
+ }
+
+ function upload(res, req){
+ 	console.log("Request handler 'show' was called.");
+ 	var form = new formidable.IncomingForm();
+    
+    console.log("about parse");
+    form.parse(req, function(error, fields, files){
+        console.log("parsing done");
+        fs.rename(files.upload.path, "img/test.jpg", function(err){
+            if(err){
+                fs.unlink("img/test.jpg");
+                fs.rename(files.upload.path, "img/test.jpg");
+            }
+        });
+       res.writeHead(200, {"Content-Type":"text/html; charset=UTF-8"});
+       res.write("送信された画像ですよー↓<br/>");
+       res.write("<img src='/show' />");
+       res.end();
+    });
+ }
+
+ function show(res){
+     console.log("Request handler 'show' was called.");
+     fs.readFile("img/test.jpg", function(error, file){
+        if(error){
+            res.writeHead(500, {"Content-Type": "text/plain; charset=UTF-8"});
+            res.write("OH MY GOD\n"+error+"\n");
+            res.end();
+        }else{
+            res.writeHead(200, {"Content-Type":"image/jpg"});
+            res.write(file);
+            res.end();
+        }
+     });
+ }
+
+ exports.start = start;
+ exports.upload = upload;
+ exports.show = show;
