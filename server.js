48c9b889942cdf38bb8fb8684507543a2f138b84
danimal141 <hideaki.ishii1204@gmail.com>
2014-11-01 09:48:34 +0900

    Add some fixtp

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var http = require("http");
 var url = require("url");
 
-function start(route, handle) {
-	function onRequest(req, res) {
+function start(route, handle){
+	function onRequest(req, res){
 		var pathname = url.parse(req.url).pathname;
 		console.log("pathname: " + pathname );
 
 		route(handle, pathname, res, req);
 	}
+
 	http.createServer(onRequest).listen(8888);
 	console.log("Server has started");
 }
