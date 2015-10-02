fece6a06f23816176170aa8ae8317f56fdfa41ca
danimal141 <hideaki.ishii1204@gmail.com>
2015-10-02 09:47:47 +0900

    Fix error

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,59 +1,63 @@
-var fs = require("fs"),
+var querystring = require("querystring"),
+    fs = require("fs"),
     formidable = require("formidable");
 
-function start(res) {
+function start(response) {
   console.log("Request handler 'start' was called.");
+
   var body = '<html>'+
     '<head>'+
     '<meta http-equiv="Content-Type" '+
     'content="text/html; charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<h1>ファイルをアップロードしてください</h1>'+
     '<form action="/upload" enctype="multipart/form-data" '+
     'method="post">'+
-    '<input type="file" name="upload">'+
-    '<input type="submit" value="Upload" />'+
+    '<input type="file" name="upload" multiple="multiple">'+
+    '<input type="submit" value="Upload file" />'+
     '</form>'+
     '</body>'+
     '</html>';
-  res.writeHead(200, {"Content-Type":"text/html"});
-  res.write(body);
-  res.end();
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
 }
 
-function upload(res, req) {
-  console.log("Request handler 'show' was called.");
-  var form = new formidable.IncomingForm();
+function upload(response, request) {
+  console.log("Request handler 'upload' was called.");
 
-  console.log("about parse");
-  form.parse(req, function(error, fields, files) {
-    console.log(files.upload.path)
-    fs.rename(files.upload.path, "img/test.jpg", function(err) {
+  var form = new formidable.IncomingForm();
+  console.log("about to parse");
+  form.parse(request, function(error, fields, files) {
+    console.log("parsing done");
+
+    /* Possible error on Windows systems:
+       tried to rename to an already existing file */
+    fs.rename(files.upload.path, "/tmp/test.png", function(err) {
       if (err) {
-        console.log(err);
-        fs.unlink("img/test.jpg");
-        fs.rename(files.upload.path, "img/test.jpg");
+        fs.unlink("/tmp/test.png");
+        fs.rename(files.upload.path, "/tmp/test.png");
       }
     });
-    res.writeHead(200, {"Content-Type":"text/html; charset=UTF-8"});
-    res.write("送信された画像ですよー↓<br/>");
-    res.write("<img src='/show' />");
-    res.end();
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("received image:<br/>");
+    response.write("<img src='/show' />");
+    response.end();
   });
 }
 
-function show(res) {
+function show(response) {
   console.log("Request handler 'show' was called.");
-  fs.readFile("img/test.jpg", function(error, file) {
-    if (error) {
-      res.writeHead(500, {"Content-Type": "text/plain; charset=UTF-8"});
-      res.write("OH MY GOD\n"+error+"\n");
-      res.end();
+  fs.readFile("/tmp/test.png", "binary", function(error, file) {
+    if(error) {
+      response.writeHead(500, {"Content-Type": "text/plain"});
+      response.write(error + "\n");
+      response.end();
     } else {
-      res.writeHead(200, {"Content-Type":"image/jpg"});
-      res.write(file);
-      res.end();
+      response.writeHead(200, {"Content-Type": "image/png"});
+      response.write(file, "binary");
+      response.end();
     }
   });
 }
